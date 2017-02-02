6fcb17c5477b6b088a4d8e1e662a0165e0f5884c
Auke Kok <sofar@foo-projects.org>
2017-02-02 15:37:37 -0800

    Add crops. (wheat, cotton).

--- init.lua
+++ init.lua
@@ -159,6 +159,22 @@ for _, node in pairs({
 	"flowers:dandelion_white",
 	"flowers:mushroom_red",
 	"flowers:mushroom_brown",
+	"farming:cotton_1",
+	"farming:cotton_2",
+	"farming:cotton_3",
+	"farming:cotton_4",
+	"farming:cotton_5",
+	"farming:cotton_6",
+	"farming:cotton_7",
+	"farming:cotton_8",
+	"farming:wheat_1",
+	"farming:wheat_2",
+	"farming:wheat_3",
+	"farming:wheat_4",
+	"farming:wheat_5",
+	"farming:wheat_6",
+	"farming:wheat_7",
+	"farming:wheat_8",
 }) do
 	flowerpot.register_node(node)
 end
